8224c9482ccb6400e052d0836d1b0b0feea4738c
Higherest <503963125@qq.com>
2023-11-21 23:32:43 +0800

    登录账号请求逻辑（一）

---
+++ Server/Hotfix/Demo/Account/Handle/C2A_LoginAccountHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ET
+{
+    public class C2A_LoginAccountHandler: AMRpcHandler<C2A_LoginAccount, A2C_LgoinAccount>
+    {
+        protected override async ETTask Run(Session session, C2A_LoginAccount request, A2C_LgoinAccount response, Action reply)
+        {
+            
+            
+            await ETTask.CompletedTask;
+        }
+    }
+}
---
+++ Server/Model/Demo/Account/Account.cs
@@ -0,0 +1,19 @@
+﻿namespace ET
+{
+    public enum AccountType
+    {
+        General = 0, //普通
+        BlackList = 1, //黑名单
+    }
+
+    public class Account: Entity, IAwake
+    {
+        public string accountName; //账户名
+
+        public string passWord; //账户密码
+
+        public long createTime; //账号创建时间
+        
+        public int accountType; //账号类型
+    }
+}
